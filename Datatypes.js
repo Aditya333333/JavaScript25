2a5702d7ba9091d19c87845c5db9abb361d840b1
Aditya <adityaraja171102@gmail.com>
2025-05-25 12:26:37 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as new version
+
+//alert(3+3)// we are using nodejs,not brower
+
+
+
+let age = 18;
+const name = "Aditya"
+let logIn = false
+let state;
+
+console.table([age,name,logIn,state])
+
+
+//number = 2 to 58
+//bigint 
+//String = ""
+//boolean = true/false
+//null
+//undefind
+
+
+//object
