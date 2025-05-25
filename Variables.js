e0674e2db58a8e416f0aa1a7d48ae3a861cf232f
Aditya <adityaraja171102@gmail.com>
2025-05-25 09:07:00 +0530

    Initial commit

---
+++ Variables.js
@@ -0,0 +1,20 @@
+const accountId = 1 //const can't change variables
+let accountEmail = "adityaraja171102@gmail.com"
+var  accountPassword = "3458"
+accountCity = "jaipur"
+let accountState;
+
+// change the variables//
+
+//accountId = 4 // not allowed
+ accountEmail ="adityaraj170802@gmail.com"
+ accountPassword = "34566"
+ accountCity = "bihar"
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
