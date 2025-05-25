6420243b26ee3e7931be24080f4f1aada37ee88e
Aditya <adityaraja171102@gmail.com>
2025-05-25 12:26:58 +0530

    ConversionandOperation

---
+++ conversionOperation.js
@@ -0,0 +1,90 @@
+//Conversion Number into String
+let num = 0
+// console.log(num)
+
+let numString = String(num)
+// console.log(numString)
+// console.log(typeof numString)
+
+// 1 = string
+
+//Conversion string into Number
+let name = undefined
+//  console.log(name)
+let nameNumber = Number(name)
+//  console.log(typeof nameNumber)
+//  console.log(nameNumber)
+
+// "aditya" = NaN
+//  "1" = 1
+// "1abc" = NaN
+//true = 1; false = 0
+//undefined = NaN
+
+//Conversion Boolearn into String
+
+let logIn = 0
+// console.log(logIn)
+
+let logInString = String(logIn)
+// console.log(typeof logInString)
+// console.log(logInString)
+
+//false = string
+// 1 = string
+
+//Conversion String into Booleran
+
+let  str = "1abc"
+// console.log(str)
+
+let  strBoolearn = Boolean(str)
+// console.log(typeof strBoolearn)
+// console.log(strBoolearn)
+
+//"aditya" = true
+// 1 = true and 0 = false
+//"" = false
+// "1abc" = ture
+
+// ********************Operation*******************************
+
+let value = 3
+let valueNumuber = -value
+console.log(valueNumuber);
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3**3);
+console.log(3%3);
+
+let srt1 = "Aditya"
+let srt2 = "raj"
+let srt3 = "Aditya" + "raj"
+console.log( srt3);
+
+
+/* These math solving not readable and it's useless      */
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("2" + 2 + 2);
+console.log(2 + 2 + "2");
+console.log(3+3*4%7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
