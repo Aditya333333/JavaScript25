1a005e6ad80ec0787bde965d4a9ffa8850801c9b
Aditya <adityaraja171102@gmail.com>
2025-05-26 22:04:29 +0530

    ImportantopicString

---
+++ String.js
@@ -0,0 +1,33 @@
+let name = "Aditya"
+let id = "21eptcs007"
+
+//console.log(name + id + "and collage"); // can't use because very old
+console.log(`my name is ${name} and collage id is ${id} `);
+
+//second type to code string 
+let gameName = new String('Adityaraj')
+// console.log(gameName);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-8,4));
+console.log(gameName.length);
+
+let UserName = "    Rahul   "
+console.log(UserName);
+console.log(UserName.trim())
+
+
+
+
+let Email = "adityaraj@gmail.com$"
+console.log(Email.replace('$',""));
+
+
+
+
+
+
+
