c42631cb3b88b992f6c5451aef1dc14710a78626
Aditya <adityaraja171102@gmail.com>
2025-05-26 09:35:01 +0530

    revision

---
+++ revision.js
@@ -0,0 +1,138 @@
+console.log("hello world");
+
+// ******************Variables*******************************************
+const collegeId = "21eptcs007" // can't change the variables
+// collegeId = "21eptcs008"
+console.log(collegeId);
+
+let name = "Aditya"
+name = "Rahul"
+console.log(name);
+
+/*  var can't use beacuse of block scope and function scope   */
+var age = 45
+age = 46
+console.log(age);
+
+/* can't use beacuse code are not readable  */
+cityName = "jaipur"
+cityName = "Bihar"
+console.log(cityName);
+
+
+// **********************Datatypes***************************************
+
+/*
+number --> 2 to 58
+bigint
+string
+boolean
+null
+
+object
+
+
+*/
+
+let num = 45
+console.log(num);
+
+let str = "Aditya"
+let bool = false
+let nll = null
+let a;
+console.table([num,str,bool,nll,a])
+// ****************** Conversion *******************************************
+//Number into string
+
+// number,bigint,string,boolean,null,object
+
+// let n = null
+// console.log(typeof n)
+// let nb =String(n)
+// console.log(typeof nb)
+// console.log(nb);
+
+//string into number
+
+// let st = null
+// console.log(typeof st);
+// let stNUM = Number(st)
+// console.log(typeof stNUM);
+// console.log(stNUM);
+
+//number into boolean
+// let n = false
+// console.log(typeof n);
+// let nBool = Boolean(n)
+// console.log(nBool);
+// console.log(typeof nBool);
+
+//string into boolean
+// let s = null
+// console.log(typeof s);
+// let sbool = Boolean(s)
+// console.log(typeof sbool);
+// console.log(sbool);
+
+//Boolean into string
+let bl = null
+console.log(bl);
+let blstr = String(bl)
+console.log(typeof blstr);
+console.log(blstr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************** Operation *******************************************
+
+// let value = 3
+// valueNum = -value
+// console.log(valueNum);
+
+// console.log(3 + 3);
+// console.log(3 -3);
+// console.log(3 *3);
+// console.log(3 **3);
+// console.log(3 /3);
+// console.log(3 %3);
+
+// code are not readable it's useless
+
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("3" + 3 +3);
+// console.log(3+3+ "3");
+// console.log(3+3*5/9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
