76d730a9b28a5a073f9147503cb19dbe1f2bf00f
Aditya <adityaraja171102@gmail.com>
2025-05-27 12:52:01 +0530

    Number and Maths topic

---
+++ NumberandMaths.js
@@ -0,0 +1,65 @@
+let score = 100
+// console.log(score);
+
+// another type write a code of number variables
+
+// go any brower inspect to console write this code and many different properties of Number
+let num = new Number(400)  
+// console.log(num)
+
+const anotherNum = 500
+// console.log(anotherNum.toString());
+// console.log(anotherNum.toString().length);
+
+
+// console.log(anotherNum.toFixed(2));
+
+// const otherNum = 25.666
+// console.log(otherNum.toPrecision(3)); // very important topic of precision
+
+const lakhs = 1000000
+console.log(lakhs);
+console.log(lakhs.toLocaleString()); //default by usa  const lakhs 1000000 = 1,00,000
+console.log(lakhs.toLocaleString('en-IN')); // India version of const lakhs 1000000 = 1,00,000
+
+
+
+
+
+
+
+// ********************************Maths*********************************************
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.ceil(2,3,5))
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4.9))
+// console.log(Math.max(4.5))
+
+
+// console.log(Math(3));
+
+// console.log(Math.random()); // random 0 to 1 
+// console.log((Math.random()*10) + 1 );
+// console.log(Math.floor(Math.random()*10) + 1 );
+
+// let min = 20
+// let max = 20
+// console.log(max);
+
+// console.log((Math.random()*(max-min + 1))+ min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
