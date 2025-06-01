52a264a048b41676e290e49621fe2a3333d2798c
Aditya <adityaraja171102@gmail.com>
2025-06-01 23:56:37 +0530

    scope

--- 03_Basic_js/Scope.js
+++ 03_Basic_js/Scope.js
@@ -9,12 +9,52 @@ if (true) {
 if (true) {
     let a =100
     const b = 2000
-    console.log("local scope:",a);
-    console.log("Inside :",b);
+    // console.log("local scope:",a);
+    // console.log("Inside :",b);
 }
 
 let a = 600
 const b = 300
-console.log("Global",a);
-console.log("Golbal",b);
+// console.log("Global",a);
+// console.log("Golbal",b);
 
+
+//******************************************Nested-scope */
+function one(num) {
+    username = "aditya"
+    function two(num) {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+one()
+
+
+if (true) {
+    username = "sita"
+    if (username === "sita") {
+        const website = "   youtube"
+        // console.log(username + website);
+        
+    }
+    
+    
+}
+// console.log(username);
+
+
+console.log(y(68));
+function y(num) {
+    return num+1
+}
+
+
+varY(67)
+const varY = function y(num1) {
+    return num1 +2
+}
+console.log(varY(98));
