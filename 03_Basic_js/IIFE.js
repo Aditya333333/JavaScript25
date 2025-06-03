997a51bb8de9051da61a102983822f96166b9a51
Aditya <adityaraja171102@gmail.com>
2025-06-03 21:34:05 +0530

    IIFEreducesGlobalproblem

---
+++ 03_Basic_js/IIFE.js
@@ -0,0 +1,16 @@
+
+
+//IIFE used for the global value to redues the complexity
+
+// function in IIFE
+(function chai() {
+    console.log(`DE CONNECTED`);
+    
+})(); // --->  IIFE 
+
+// ArrowFunction in IIFE
+
+(() => {
+    console.log(`DE CONNECTED TWO`);
+    
+})()
