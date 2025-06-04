ca8528a354eeb85efef379737c38ad239c1b8503
Aditya <adityaraja171102@gmail.com>
2025-06-04 11:10:21 +0530

    Switchcase

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,44 @@
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case");
+        
+        break;
+}
+
+//string datatype
+const  name = "aditya"
+
+switch (name) {
+    case "adityaraja":
+        console.log("adityaraj");
+        break;
+    case "Rahul":
+        console.log("Rahul");
+        break;
+    case "Neha":
+        console.log("Neha");
+        break;
+    case "aditya":
+        console.log("aditya");
+        break;    
+
+
+    default:
+    console.log("default case");    
+    break;
+}
