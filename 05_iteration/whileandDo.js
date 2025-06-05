b65e7e9efcb5d2c89773452c8f52a8273ff1231f
Aditya <adityaraja171102@gmail.com>
2025-06-05 21:30:59 +0530

    WhileLoop and Do While Loop

---
+++ 05_iteration/whileandDo.js
@@ -0,0 +1,20 @@
+// let i = 1;
+// while (i <=10) {
+//     console.log(i);
+//      i = i + 1
+// }
+
+ let Myarray = ["Aditya","Rahul","Neha"]
+ let arr = 0
+ while (arr < Myarray.length) {
+    console.log(`value of arr ${Myarray[arr]}`);
+    arr = arr + 1
+ }
+
+
+
+// let j =11
+// do {
+//     console.log(`value of j ${j}`);
+//     j++
+// } while (j<=10);
