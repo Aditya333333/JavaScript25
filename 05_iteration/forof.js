81081d9803ff97a25e0bbfe56dab2b30cc1ac9f8
Aditya <adityaraja171102@gmail.com>
2025-06-06 10:06:41 +0530

    ForofandMap in js

---
+++ 05_iteration/forof.js
@@ -0,0 +1,29 @@
+
+// Array
+let Myarray = [1,2,3,4,5,6]
+for (const arr of Myarray) {
+    // console.log(arr);
+    
+}
+
+//Stirng
+let Mystring = "Helloworld"
+for (const str of Mystring) {
+    // console.log(str);
+    
+}
+
+// Maps
+
+let map = new Map()                 // map only unique value 
+map.set("IN","INDIAN")
+map.set("FR","FRANCE")
+map.set("JP","JAPAN")
+map.set("IN","INDIAN")
+// console.log(map);
+
+for (const Mymap of map) {
+    console.log(`map is  a ${Mymap}`);
+    
+}
+
