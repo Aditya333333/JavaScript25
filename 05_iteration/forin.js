031b8d4789ebda210458fca13ab4fd9ee3aaec94
Aditya <adityaraja171102@gmail.com>
2025-06-06 10:33:46 +0530

    forINusedspecifyobjectkaliya

---
+++ 05_iteration/forin.js
@@ -0,0 +1,27 @@
+
+//forin specifly used a forin loop
+
+let Myobject = {
+    js : "javascript",
+    cpp : "c++",
+    py : "python"
+}
+for (const key in Myobject) {
+//    console.log(`${key} is a shortcut of ${Myobject[key]}`);
+   
+}
+
+let Myarray = ["aditya",2,"Rahul",4,"Neha",6]
+for (const key in Myarray) {
+    //   console.log(`${key} is a  ${Myarray[key]}`);
+
+  
+}
+
+let Mystring = "Helloworld"
+for (const key in Mystring) {
+        // console.log(`${key} is  ${Mystring[key]}`);
+
+}
+
+
