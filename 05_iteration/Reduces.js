7d8fbded482af3342e31837281a76a0f45e00029
Aditya <adityaraja171102@gmail.com>
2025-06-08 11:32:42 +0530

    PaymentgateProces.

--- 05_iteration/Reduces.js
+++ 05_iteration/Reduces.js
@@ -6,5 +6,25 @@ let arrNew = arr.reduce((acc,currval) => {
     return acc + currval
 },0);
 const Hotel = arr.reduce((acc,curr) => acc +curr,0)
-console.log(arrNew);
+// console.log(arrNew);
 
+let shoppingCards = [
+    {
+        itemName : "java course",
+        Price : 1999
+    },
+     {
+        itemName : "js course",
+        Price : 2999
+    },
+     {
+        itemName : "React native course",
+        Price : 19999
+    },
+     {
+        itemName : "python course",
+        Price : 999
+    },
+]
+let pay = shoppingCards.reduce( (acc,item) => (item.Price + acc ),0)
+console.log(pay);
